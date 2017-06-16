3afa6adb5b17f9afabe2457a1d859e038d1d893f
J.C. ★ YIS <J.C.  YIS>
2017-06-16 20:59:22 +0800

    V.2.0

--- gameskills.js
+++ gameskills.js
@@ -1,31 +1,163 @@
- var skills = [];
-//=======================================================================================================
-skills[0] = "SKILL_0";		
-skills[1000] = "SKILL_1000";
-//=======================================================================================================
-skills[1] = "hit";
-function s1(){
-	sklfontformat.fill = 'gray';
+function manstyledecide(manstyle){
+	switch(manstyle){
+		case "standard":
+			mansetstandard();
+			document.getElementById("manconfirmed").innerHTML = "done!";
+			break;
+
+		case "sniper":
+			mansetsniper();
+			document.getElementById("manconfirmed").innerHTML = "done!";
+			break;
+
+		case "machinegun":
+			mansetmachinegun();
+			document.getElementById("manconfirmed").innerHTML = "done!";
+			break;
+
+		default:
+			document.getElementById("manconfirmed").innerHTML = "ERROR! Please report to J.C.";
+			break;
+	}
 }
-//=======================================================================================================
-skills[2] = "bullet";
-function s2(){
 
+function mansetstandard() {
+			manlv = 1;
+			manexp = 0;
+			manstyle = "Standard";
+			manatk = 500;
+			manmaxhp = 500;
+			manhpcure = 200;
+			manmppunish = 100;		
+			manshootspd = 400;
+			manshootrate = 200;
+			manshootvariance = 3;
+			manmpuse = 0.5;
+			mandef = 100;
+			manmaxmp = 500;
+			manmpcure = 200;
+			//mansskill = 0;
+			reinit();
 }
 
-function s2f(){
 
+function mansetsniper() {
+			manlv = 1;
+			manexp = 0;
+			manstyle = "Sniper";
+			manatk = 4000;
+			manmaxhp = 420;
+			manhpcure = 230;
+			manmppunish = 170;		
+			manshootspd = 800;
+			manshootrate = 1000;
+			manshootvariance = 0;
+			manmpuse = 1.0;
+			mandef = 87;
+			manmaxmp = 520;
+			manmpcure = 250;
+			//mansskill = 0;
+			reinit();
 }
-//=======================================================================================================
-//=======================================================================================================
-//=======================================================================================================
-//=======================================================================================================
-//=======================================================================================================
-//=======================================================================================================
-//=======================================================================================================
-//=======================================================================================================
+
+function mansetmachinegun() {
+			manlv = 1;
+			manexp = 0;
+			manstyle = "Machine Gun";
+			manatk = 150;
+			manmaxhp = 505;
+			manhpcure = 200;
+			manmppunish = 115;		
+			manshootspd = 250;
+			manshootrate = 100;
+			manshootvariance = 12;
+			manmpuse = 0.25;
+			mandef = 90;
+			manmaxmp = 666;
+			manmpcure = 287;
+			//mansskill = 0;
+			reinit();
+}
 
 
-//=======================================================================================================
-//=======================================================================================================
-//=======================================================================================================
+//======================FOE===========================================================================================================================
+function foestyledecide(foestyle){
+	switch(foestyle){
+		case "standard":
+			foesetstandard();
+			document.getElementById("foeconfirmed").innerHTML = "done!";
+			break;
+
+		case "sniper":
+			foesetsniper();
+			document.getElementById("foeconfirmed").innerHTML = "done!";
+			break;
+
+		case "machinegun":
+			foesetmachinegun();
+			document.getElementById("foeconfirmed").innerHTML = "done!";
+			break;
+
+		default:
+			document.getElementById("foeconfirmed").innerHTML = "ERROR! Please report to J.C.";
+			break;
+	}
+}
+
+function foesetstandard() {
+			foelv = 1;
+			foeexp = 0;
+			foestyle = "Standard";
+			foeatk = 500;
+			foemaxhp = 500;
+			foehpcure = 200;		
+			foeshootspd = 400;
+			foeshootrate = 200;
+			foeshootvariance = 3;
+			foempuse = 0.3;
+			foedef = 100;
+			foemaxmp = 500;
+			foempcure = 200;
+			//foesskill = 0;
+			reinit();
+}
+
+
+function foesetsniper() {
+			foelv = 1;
+			foeexp = 0;
+			foestyle = "Sniper";
+			foeatk = 4000;
+			foemaxhp = 420;
+			foehpcure = 230;		
+			foeshootspd = 800;
+			foeshootrate = 1000;
+			foeshootvariance = 0;
+			foempuse = 0.9;
+			foedef = 87;
+			foemaxmp = 520;
+			foempcure = 250;
+			//foesskill = 0;
+			reinit();
+}
+
+function foesetmachinegun() {
+			foelv = 1;
+			foeexp = 0;
+			foestyle = "Machine Gun";
+			foeatk = 300;
+			foemaxhp = 505;
+			foehpcure = 200;		
+			foeshootspd = 250;
+			foeshootrate = 100;
+			foeshootvariance = 12;
+			foempuse = 0.2;
+			foedef = 90;
+			foemaxmp = 666;
+			foempcure = 287;
+			//foesskill = 0;
+			reinit();
+}
+
+
+//======================FOE===========================================================================================================================
